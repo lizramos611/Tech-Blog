4636a3a6d369b6c0f3f23967396a436b687b5d1f
lizramos611 <liz.ramos@aim.com>
2023-03-20 14:14:15 -0400

    fixing server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const routes = require('./controller');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +16,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log('Now Listening!'))
