70ac0e9f6c15a0951fd8d97ee944c1f2bf87e569
lizramos611 <liz.ramos@aim.com>
2023-03-20 12:52:49 -0400

    adding in exports

--- model/index.js
+++ model/index.js
@@ -1,2 +1,14 @@
 const Blogs = require('./blogs');
 const User = require('./user');
+//connecting blogs and users
+Blogs.belongsTo(User, {
+    foreignKey: 'post_writer'
+});
+
+User.hasMany(Blogs, {
+    foreignKey: 'post_writer',
+    onDelete: 'CASCADE',
+});
+
+//exporting blogs and users
+module.exports = {User, Blogs}; 
