2a37bb6139827d3341474ed2ecf72cbfac979683
lizramos611 <liz.ramos@aim.com>
2023-03-20 14:19:28 -0400

    setting up handlebars for blog post

--- public/js/new-post.js
+++ public/js/new-post.js
@@ -2,5 +2,10 @@ const newPost = async (event) => {
     event.preventDefault();
     //so page doesnt reset 
 
-    const postTitle = document
+    const postTitle = document.querySelector('#post-title').value.trim();
+    const postMessage = document.querySelector('#post-message').value.trim();
+
+    if (postTitle && postMessage) {
+        const response = await fetch ('/api/posts')
+    }
 }
