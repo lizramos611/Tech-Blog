6c1bf14aa40e82a322609936d97a2c28dc7b3cfa
lizramos611 <liz.ramos@aim.com>
2023-03-20 13:45:42 -0400

    adding in responses for signup page

--- public/js/login-signup.js
+++ public/js/login-signup.js
@@ -21,5 +21,19 @@ const signup = async (event) => {
 
     const email = document.querySelector('#email-signup').value.trim();
     const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup')
-}
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (email && username && password ) {
+        const response = await fetch('/api/users, ' {
+            method: 'POST',
+            body: JSON.stringify({email, username, password}),
+            headers: { 'Content-Type': 'application/json'},
+
+        });
+
+        if (response.ok) {
+            document.location.replace('/dash');
+        }
+    }
+};
+
